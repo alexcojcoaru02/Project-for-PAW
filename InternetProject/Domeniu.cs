be6926fa0c52684e00234936582ccd85cc6ba4b5
Alex Cojocaru <cojocaruclaudiu20@stud.ase.ro>
2022-05-18 01:38:08 +0300

    Create initial project + 2 Clases

---
+++ InternetProject/Domeniu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetProject
+{
+    class Domeniu
+    {
+        public string Nume { get; set; }
+        public string Proprietar { get; set; }
+    }
+}
---
+++ InternetProject/Vizite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetProject
+{
+    class Vizite
+    {
+        public string IP { get; set; }
+        public string Adresa { get; set; }
+        public DateTime Timp { get; set; }
+    }
+}
